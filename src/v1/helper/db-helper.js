8df0007ebc9b71abd8f3f056056f7b6dce96dd26
Vibhav-Deo <vdeo@deakin.edu.au>
2019-03-16 21:42:03 +1100

    changes to db-helper added generic functionalities

--- src/v1/helper/db-helper.js
+++ src/v1/helper/db-helper.js
@@ -3,7 +3,8 @@
  */
 const mongoose = require('mongoose');
 const CONSTANTS = require('./constants');
-
+const model = require('../models');
+mongoose.Promise = global.Promise;
 var dbConnection = async () => {
     // get reference to database
     var db = mongoose.connection;
@@ -21,9 +22,89 @@ var dbConnection = async () => {
 var closeConnection = () => {
     mongoose.Connection.close();
 }
+
+var getRoomData = () => {
+    var devices = [];
+    model.roomModel.find({})
+        .then(res => {
+            res.forEach(element => {
+                devices.push(element)
+            })
+        })
+        .then(() => {
+            return devices;
+        })
+        .catch(error => {
+            return error;
+        })
+}
+var insertDataToRoom = (data) => {
+    const roomModel = model.roomModel;
+    const room = new roomModel({
+        roomId: data.roomId,
+        deviceId: data.deviceId
+    })
+
+    room.save(function (err) {
+        if (err) return CONSTANTS.HTTP_INTERNAL_ERROR;
+        else return CONSTANTS.HTTP_CREATED;
+    });
+}
+var deleteDataFromRoom = (deviceId) => {
+    model.roomModel.deleteOne({ deviceId: deviceId })
+        .then(res => {
+            return CONSTANTS.HTTP_OK;
+        })
+        .catch(error => {
+            return error;
+        })
+}
+
+var getuserActvity = () => {
+    var userActivity = [];
+    model.userActivityModel.find({})
+        .then(res => {
+            res.forEach(element => {
+                userActivity.push(element)
+            })
+        })
+        .then(() => {
+            return userActivity
+        })
+        .catch(error => {
+            return error;
+        })
+}
+var insertDataToUserActivity = (data) => {
+    const userActivityModel = model.userActivityModel;
+    const userActivity = new userActivityModel({
+        userId: data.userId,
+        deviceId: data.deviceId,
+        timestamp: data.timestamp
+    })
+    userActivity.save(function (err) {
+        if (err) return CONSTANTS.HTTP_INTERNAL_ERROR;
+        else return CONSTANTS.HTTP_CREATED;
+    });
+}
+var deleteDataFromUserActivity = (userId) => {
+    model.userActivityModel.deleteOne({ userId: userId })
+        .then(res => {
+            return CONSTANTS.HTTP_OK;
+        })
+        .catch(error => {
+            return error;
+        })
+}
 var DBHELPER = {
     dbConnection: dbConnection,
-    closeConnection: closeConnection
+    closeConnection: closeConnection,
+    getRoomData: getRoomData,
+    insertDataToRoom: insertDataToRoom,
+    deleteDataFromRoom: deleteDataFromRoom,
+    insertDataToUserActivity: insertDataToUserActivity,
+    deleteDataFromUserActivity: deleteDataFromUserActivity,
+    getuserActvity: getuserActvity
 }
 
 module.exports = DBHELPER
